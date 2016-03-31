ca37957d33e9ad7e74ecfc06c4de72d54aeb1f98
HokLee <herolisoinlove@163.com>
2016-03-31 15:26:47 +0800

    初创项目

---
+++ app/src/main/java/graduation/ljh/com/pgraduation_easy_english/MainActivity.java
@@ -0,0 +1,13 @@
+package graduation.ljh.com.pgraduation_easy_english;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
